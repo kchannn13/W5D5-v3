d5fbd009a5a8fa33ae75f38dbbd111d59824ecf3
Shan Kim <shankim35@gmail.com>
2023-02-10 16:04:23 -0500

    02_queries

--- movie_buff/02_queries.rb
+++ movie_buff/02_queries.rb
@@ -1,12 +1,13 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
+  Movie.where(yr: 1980..1989).where(score: 3..5).select("movies.id, movies.title, movies.yr, movies.score")
   
 end
 
 def bad_years
-  # List the years in which no movie with a rating above 8 was released.
-  
+  # List the years in which no movie with a rating above 8 was released.\
+  Movie.where('score <= 8').select(:yr).distinct.pluck(:yr)  
 end
 
 def cast_list(title)
